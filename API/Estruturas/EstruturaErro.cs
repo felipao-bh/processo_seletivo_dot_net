bc85a838c575d3e160d446e7f26ffd071f775162
fferreira <felipe.ferreira@tty2000.com.br>
2021-09-26 22:18:35 -0300

    Commit CSU.API.NFE.RetornaNFE

---
+++ API/Estruturas/EstruturaErro.cs
@@ -0,0 +1,21 @@
+﻿namespace CSU.API.NFE.RetornaNFE.Estruturas
+{
+    public class EstruturaErro
+    {
+
+        /// <summary>
+        /// Atributo para indicar qual o tipo de erro:        
+        /// </summary>
+        public short IndicadorErro { get; set; }
+
+        /// <summary>
+        /// Atributo com o código do erro que ocorreu.         
+        /// </summary>
+        public string CodigoErro { get; set; }
+
+        /// <summary>
+        /// Atributo com a descrição do erro que ocorreu.         
+        /// </summary>
+        public string DescricaoErro { get; set; }
+    }
+}
---
+++ API/SQL/NegocioSQL.cs
@@ -0,0 +1,83 @@
+﻿using CSU.API.NFE.RetornaNFE.Entidades;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CSU.API.NFE.RetornaNFE.SQL
+{
+    public class NegocioSQL
+    {
+        #region [Propriedades]
+
+        private string _connectionStringSQL;
+
+        #endregion
+
+        #region [Construtores]
+
+        /// <summary>
+        /// Construtor
+        /// </summary>
+        public NegocioSQL(IConfiguration iconfiguration)
+        {
+            try
+            {
+                _connectionStringSQL = iconfiguration.GetConnectionString("Default");
+            }
+            catch (Exception erro)
+            {
+                throw erro;
+            }
+        }
+
+        #endregion
+
+        #region [ConsultaNFE]
+
+        /// <summary>
+        /// CONSULTA 01 – ConsultaNFE
+        /// </summary>
+        /// <param name="mesBusca"></param>
+        /// <returns>Lista de NFE'S</returns>
+        public List<EstruturaNFE> ConsultaNFE(int mesBusca)
+        {
+            List<EstruturaNFE> ListaNotasFiscais = new List<EstruturaNFE>();
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(_connectionStringSQL))
+                {
+                    SqlCommand cmd = new SqlCommand("SP_RetornaNF_Por_Mes", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    SqlParameter _mesBusca = cmd.Parameters.AddWithValue("@MesBusca", mesBusca);
+                    con.Open();
+
+                    SqlDataReader rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        ListaNotasFiscais.Add(new EstruturaNFE
+                        {
+                            COD_NOTA = rdr[0].ToString(),
+                            COD_VENDA = rdr[1].ToString(),
+                            DESTINATARIO_REMETENTE = rdr[2].ToString(),
+                            DT_EMISSAO = rdr[3].ToString(),
+                            DT_SAIDA_ENTRADA = rdr[4].ToString(),
+                            NUM_NOTA = rdr[5].ToString()
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro na RetornaNFE – (SQL_SERVER) " + ex.Message);
+            }
+
+            return ListaNotasFiscais;
+        }
+
+        #endregion
+    }
+}
